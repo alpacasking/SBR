85c58245cf3f8f57f04665eef11c298ee1ffd973
alpacasking <hotancle@163.com>
2019-11-23 15:21:12 +0800

    first commit

---
+++ Assets/SBR/Scripts/StrokePainting.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering;
+using System.Runtime.InteropServices;
+[Serializable]
+[PostProcess(typeof(StrokePaintingRenderer), PostProcessEvent.AfterStack, "Alpacasking/StrokePainting")]
+public sealed class StrokePainting : PostProcessEffectSettings
+{
+    [Tooltip("Canvas texture used to paint."), DisplayName("Canvas")]
+    public TextureParameter canvasTexture = new TextureParameter { value = null };
+
+    [Tooltip("Stroke texture used to paint."), DisplayName("Stoke")]
+    public TextureParameter strokeTexture = new TextureParameter { value = null };
+
+    [Range(0.001f, 1), Tooltip("Stroke size used to paint."), DisplayName("Stoke Size")]
+    public FloatParameter strokeSize = new FloatParameter { value = 0.01f };
+
+    [UnityEngine.Rendering.PostProcessing.Min(1), Tooltip("Stroke interval used to paint."), DisplayName("Stoke Interval")]
+    public IntParameter strokeInterval = new IntParameter { value = 5 };
+    public override bool IsEnabledAndSupported(PostProcessRenderContext context)
+    {
+        return enabled.value
+            && canvasTexture.value != null && strokeTexture != null;
+    }
+}
+
+public struct StrokeData
+{
+    public Vector4 pos;
+    public Color color;
+
+    public Vector4 eigens;
+};
+
+[UnityEngine.Scripting.Preserve]
+public sealed class StrokePaintingRenderer : PostProcessEffectRenderer<StrokePainting>
+{
+    private ComputeShader strokeComputeShader;
+
+    private ComputeShader grayScaleShader;
+
+    private ComputeShader tensorShader;
+
+    private ComputeShader gaussianBlurShader;
+
+    private ComputeShader bilateralFilterShader;
+
+    private ComputeShader eigenShader;
+
+    private ComputeBuffer strokeBuffer;
+
+    private ComputeBuffer strokeArgsBuffer;
+
+    private int[] args = new int[] { 0, 1, 0, 0 };
+
+    private int generateStrokeKernelID;
+
+    private int toGrayScaleKernelID;
+
+    private int toTensorKernelID;
+
+    private int gaussianBlurHorizontalKernelID;
+
+    private int gaussianBlurVerticalKernelID;
+
+    private int bilateralFilterHorizontalKernelID;
+
+    private int bilateralFilterVerticalKernelID;
+
+
+    private int eigenKernelID;
+
+    private int Stride;
+
+    private Material strokeRenderMat;
+
+    private int tempRTID1;
+
+    private int tempRTID2;
+
+    private int tempCanvas;
+
+    public override void Init()
+    {
+        strokeComputeShader = (ComputeShader)Resources.Load("ComputeShaders/GenerateStroke");
+        grayScaleShader = (ComputeShader)Resources.Load("ComputeShaders/GrayScale");
+        tensorShader = (ComputeShader)Resources.Load("ComputeShaders/Tensor");
+        gaussianBlurShader = (ComputeShader)Resources.Load("ComputeShaders/GaussianBlur");
+        bilateralFilterShader = (ComputeShader)Resources.Load("ComputeShaders/BilateralFilter");
+        eigenShader = (ComputeShader)Resources.Load("ComputeShaders/Eigen");
+        generateStrokeKernelID = strokeComputeShader.FindKernel("GenerateStroke");
+        toGrayScaleKernelID = grayScaleShader.FindKernel("ToGrayScale");
+        toTensorKernelID = tensorShader.FindKernel("ToTensor");
+        gaussianBlurHorizontalKernelID = gaussianBlurShader.FindKernel("GaussianBlurHorizontal");
+        gaussianBlurVerticalKernelID = gaussianBlurShader.FindKernel("GaussianBlurVertical");
+        bilateralFilterHorizontalKernelID = bilateralFilterShader.FindKernel("BilateralFilterHorizontal");
+        bilateralFilterVerticalKernelID = bilateralFilterShader.FindKernel("BilateralFilterVertical");
+        eigenKernelID = eigenShader.FindKernel("Eigen");
+        Stride = Marshal.SizeOf(typeof(StrokeData));
+        strokeBuffer = new ComputeBuffer(Screen.width * Screen.height, Stride, ComputeBufferType.Append);
+        strokeArgsBuffer = new ComputeBuffer(4, sizeof(int), ComputeBufferType.IndirectArguments);
+        strokeArgsBuffer.SetData(args);
+        strokeRenderMat = new Material(Shader.Find("Hidden/RenderStroke"));
+        tempRTID1 = Shader.PropertyToID("tempRTID1");
+        tempRTID2 = Shader.PropertyToID("tempRTID2");
+        tempCanvas = Shader.PropertyToID("tempCanvas");
+    }
+    public override void Release()
+    {
+        strokeBuffer.Release();
+        strokeArgsBuffer.Release();
+    }
+
+    public override void Render(PostProcessRenderContext context)
+    {
+        strokeBuffer.SetCounterValue(0);
+        strokeRenderMat.SetFloat("_StrokeSize", settings.strokeSize.value);
+        var cmd = context.command;
+        cmd.BeginSample(" StrokePainting");
+        RenderTextureDescriptor desc = new RenderTextureDescriptor(context.width, context.height);
+        desc.enableRandomWrite = true;
+        cmd.GetTemporaryRT(tempCanvas, desc);
+        desc.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R32G32B32A32_SFloat;
+        cmd.GetTemporaryRT(tempRTID1, desc);
+        cmd.GetTemporaryRT(tempRTID2, desc);
+
+        //To GrayScale
+        cmd.SetComputeTextureParam(grayScaleShader, toGrayScaleKernelID, "Source", context.source);
+        cmd.SetComputeTextureParam(grayScaleShader, toGrayScaleKernelID, "Result", tempRTID1);
+        cmd.DispatchCompute(grayScaleShader, toGrayScaleKernelID, context.width, context.height, 1);
+        //cmd.Blit(tempGrayScaleRTID, context.destination);
+
+        //calculate tensor
+        cmd.SetComputeTextureParam(tensorShader, toTensorKernelID, "Source", tempRTID1);
+        cmd.SetComputeTextureParam(tensorShader, toTensorKernelID, "Result", tempRTID2);
+        cmd.DispatchCompute(tensorShader, toTensorKernelID, context.width, context.height, 1);
+        //cmd.Blit(tempRTID2, context.destination);
+
+        //gaussian blur
+        cmd.SetComputeIntParam(gaussianBlurShader, "WindowSize", 4);
+        cmd.SetComputeFloatParam(gaussianBlurShader, "Sigma", 0.01f);
+        cmd.SetComputeTextureParam(gaussianBlurShader, gaussianBlurHorizontalKernelID, "Source", tempRTID2);
+        cmd.SetComputeTextureParam(gaussianBlurShader, gaussianBlurHorizontalKernelID, "Result", tempRTID1);
+        cmd.DispatchCompute(gaussianBlurShader, gaussianBlurHorizontalKernelID, context.width, context.height, 1);
+        cmd.SetComputeTextureParam(gaussianBlurShader, gaussianBlurHorizontalKernelID, "Source", tempRTID1);
+        cmd.SetComputeTextureParam(gaussianBlurShader, gaussianBlurHorizontalKernelID, "Result", tempRTID2);
+        cmd.DispatchCompute(gaussianBlurShader, gaussianBlurHorizontalKernelID, context.width, context.height, 1);
+        //cmd.Blit(tempRTID2, context.destination);
+
+        // eigen
+        cmd.SetComputeTextureParam(eigenShader, eigenKernelID, "Source", tempRTID2);
+        cmd.SetComputeTextureParam(eigenShader, eigenKernelID, "Result", tempRTID1);
+        cmd.DispatchCompute(eigenShader, eigenKernelID, context.width, context.height, 1);
+        //cmd.Blit(tempRTID1, context.destination);
+
+
+        // Generate strokes
+        cmd.Blit(settings.canvasTexture.value, tempCanvas);
+        cmd.Blit(context.source, context.destination);
+        cmd.SetRenderTarget(tempCanvas);
+        cmd.SetComputeTextureParam(strokeComputeShader, generateStrokeKernelID, "Source", context.source);
+        cmd.SetComputeTextureParam(strokeComputeShader, generateStrokeKernelID, "Eigens", tempRTID1);
+        cmd.SetComputeIntParam(strokeComputeShader, "StrokeInterval", settings.strokeInterval.value);
+        cmd.SetComputeBufferParam(strokeComputeShader, generateStrokeKernelID, "GeneratedStrokes", strokeBuffer);
+        cmd.DispatchCompute(strokeComputeShader, generateStrokeKernelID, context.width, context.height, 1);
+        cmd.CopyCounterValue(strokeBuffer, strokeArgsBuffer, 0);
+        strokeRenderMat.SetBuffer("inputBuffer", strokeBuffer);
+        strokeRenderMat.SetTexture("_MainTex", settings.strokeTexture.value);
+        cmd.DrawProceduralIndirect(Matrix4x4.identity, strokeRenderMat, 0, MeshTopology.Points, strokeArgsBuffer);
+        cmd.CopyTexture(tempCanvas, 0, 0, context.width / 2, 0, context.width / 2, context.height, context.destination, 0, 0, context.width / 2, 0);
+
+        cmd.SetRenderTarget(context.destination);
+        cmd.ReleaseTemporaryRT(tempRTID1);
+        cmd.ReleaseTemporaryRT(tempRTID2);
+        cmd.ReleaseTemporaryRT(tempCanvas);
+        cmd.EndSample("StrokePainting");
+    }
+}
---
+++ Assets/SBR/Shaders/RenderStroke.shader
@@ -0,0 +1,106 @@
+﻿Shader "Hidden/RenderStroke"
+{
+
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+        Blendop Min
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma geometry geom
+
+            #include "UnityCG.cginc"
+
+            struct strokeData {
+                float4 pos;
+                float4 color;
+                float4 eigens;
+            };
+
+            struct v2g
+            {
+                float4 pos : SV_POSITION;
+                float4 color : COLOR0;
+                float4 eigens: COLOR1;
+            };
+            
+            struct g2f
+            {
+                float4 pos : SV_POSITION;
+                float4 color : COLOR0;
+                float2 uv : TEXCOORD0;
+            };
+
+            StructuredBuffer<strokeData> inputBuffer;
+            sampler2D _MainTex;
+            float _StrokeSize;
+
+            v2g vert (uint id : SV_VertexID)
+            {
+                v2g o;
+                o.color = inputBuffer[id].color;
+                float4 tempPos = inputBuffer[id].pos;
+                tempPos.x = 2.0*tempPos.x/_ScreenParams.x - 1.0;
+                tempPos.y = 2.0*(_ScreenParams.y-tempPos.y)/_ScreenParams.y - 1.0;
+
+                o.pos = tempPos;
+                o.eigens = inputBuffer[id].eigens;
+                return o;
+            }
+
+            [maxvertexcount(4)]
+            void geom (point v2g input[1], inout TriangleStream<g2f> outStream)
+            {
+                float2x2 rotateMatrix;
+                rotateMatrix[0][0] = input[0].eigens.x;
+                rotateMatrix[0][1] = -input[0].eigens.y;
+                rotateMatrix[1][0] = input[0].eigens.y;
+                rotateMatrix[1][1] = input[0].eigens.x;
+                
+                float4 pos = input[0].pos;
+                float ratio = _ScreenParams.x / _ScreenParams.y;
+                float dx = _StrokeSize;
+                float dy = _StrokeSize*0.5;
+                g2f output;
+                output.color=input[0].color;
+                float2 temp = mul(float2(-dx, dy),rotateMatrix );
+                temp.y= temp.y*ratio;
+                output.pos = float4(temp+pos.xy,pos.z,pos.w);
+                output.uv = float2(0,0);
+                outStream.Append(output);
+
+                temp = mul(float2(dx, dy),rotateMatrix );
+                temp.y= temp.y*ratio;
+                output.pos = float4(temp+pos.xy,pos.z,pos.w);
+                output.uv = float2(1,0);
+                outStream.Append(output);
+
+                temp = mul( float2(-dx, -dy),rotateMatrix);
+                temp.y= temp.y*ratio;
+                output.pos = float4(temp+pos.xy,pos.z,pos.w);
+                output.uv = float2(0,1);
+                outStream.Append(output);
+
+                temp = mul( float2(dx, -dy),rotateMatrix);
+                temp.y= temp.y*ratio;
+                output.pos = float4(temp+pos.xy,pos.z,pos.w);
+                output.uv = float2(1,1);
+                outStream.Append(output);
+
+                outStream.RestartStrip();
+            }
+
+            float4 frag (g2f i) : COLOR
+            {
+                float4 sc = tex2D(_MainTex,i.uv);
+                float3 tempColor = i.color.rgb;
+                return float4(lerp(1,tempColor.r,1-sc.r),lerp(1,tempColor.g,1-sc.r),lerp(1,tempColor.b,1-sc.r),1-sc.r); 
+            }
+            ENDCG
+        }
+    }
+}
